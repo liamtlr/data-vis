6dbfb80967225b601ce0ccfd5d0fcdb77f395208
Liam Taylor <liam.taylor888@googlemail.com>
2017-04-12 21:34:20 +0100

    reformatted scale for dates

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 // customer journey stuff
 
-var barData = [{value: 20, text: 'Event', date: '25-01-17', event_detail: "checkout view" },
-			  {value: 50, text: 'Impression', date: '26-01-17', event_detail: "email sign up" },
-			  {value: 85, text: 'Event', date: '22-02-17', event_detail: "item view" },
-			  {value: 15, text: 'Event', date: '23-02-17', event_detail: "item view" },
-			  {value: 70, text: 'Impression', date: '03-03-17', event_detail: "checkout view" },
-			  {value: 20, text: 'Conversion', date: '03-03-17', event_detail: "email sign up"},
-			  {value: 245, text: 'Conversion 2', date: '16-04-17', event_detail: "email sign up"},
-			  {value: 20, text: 'Event', date: '25-01-17', event_detail: "item view" },
-			  {value: 50, text: 'Impression', date: '26-01-17', event_detail: "item view" },
-			  {value: 85, text: 'Event', date: '22-02-17', event_detail: "checkout view" },
-			  {value: 15, text: 'Event', date: '23-02-17', event_detail: "item view"},
-			  {value: 70, text: 'Impression', date: '03-03-17', event_detail: "email sign up"},
-			  {value: 20, text: 'Conversion', date: '03-03-17', event_detail: "checkout view" },
-			  {value: 245, text: 'Conversion 2', date: '16-04-17', event_detail: "PURCHASE"}]
+var barData = [{value: 20, text: 'Event', date: new Date(2017, 0, 14), event_detail: "checkout view" },
+			  {value: 50, text: 'Impression', date: new Date(2017, 0, 17), event_detail: "email sign up" },
+			  {value: 85, text: 'Event', date: new Date(2017, 01, 23), event_detail: "item view" },
+			  {value: 15, text: 'Event', date: new Date(2017, 03, 14), event_detail: "item view" },
+			  {value: 70, text: 'Impression', date: new Date(2017, 04, 2), event_detail: "checkout view" },
+			  {value: 20, text: 'Conversion', date: new Date(2017, 04, 3), event_detail: "email sign up"},
+			  {value: 245, text: 'Conversion 2', date: new Date(2017, 04, 5), event_detail: "email sign up"}]
 
 var height = 800,
 	width = 600,
 	radius = 5
-	xCoord = 100
+	xCoord = 350
 	yCoord = 20
 	circleOffset = 50,
 	rectWidth = 2
+
+var minDate = barData[0].date
+var maxDate = barData[barData.length - 1].date
 
 var yScale = d3.scaleBand()
 			   .domain(d3.range(0, barData.length))
 			   .range([20, height - 20])
 			   .paddingInner(.8)
+
+// Below requires that dates are sorted chronologically from DB query
+
+var yDateScale = d3.scaleTime()
+    .domain([minDate, maxDate])
+    .range([20, height - 20]);
+
+
 
 var yScaleRect = d3.scaleLinear()
 				   .range([20, height-20])
@@ -44,7 +48,7 @@ d3.select('#chart')
 	  .enter().append('circle')
 	  	.classed('event-circle', true)
 	  	.attr('cy', function(d,i){
-  			return yScale(i);
+  			return yDateScale(d.date);
   		})
 	  	.attr('cx', xCoord)
 	  	.attr('r', function(d, i){
@@ -75,14 +79,14 @@ d3.select('svg#customer-journey')
   	})
   	.attr('x', xCoord + 25)
   	.attr('y', function(d,i){
-  		return yScale(i) + 5;
+  		return yDateScale(d.date) + 5;
   	})
   	.on('mouseover', function(d){
   		toolTip.transition()
   			   .style('opacity', .9)
   			   .style('left', (d3.event.pageX - 35) + 'px')
   			   .style('top', (d3.event.pageY -30) + 'px')
-  		toolTip.html(d.event_detail) 
+  		toolTip.html(d.event_detail)
   	})
   	.on('mouseout', function(d){
   		toolTip.transition()
@@ -110,7 +114,7 @@ d3.select('svg#customer-journey')
   	.attr('text-anchor', 'end')
   	.attr('x', xCoord -25)
   	.attr('y', function(d,i){
-  		return yScale(i) + 5;
+  		return yDateScale(d.date) + 5;
   	})
 
 d3.select('svg#customer-journey').append('svg')
